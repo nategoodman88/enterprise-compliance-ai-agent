f6192189d1bbfff4041cfcf14c6add1436093a02
nate.goodman <nate.goodman@sparkbusinessworks.com>
2026-08-18 11:46:11 -0400

    fix: deployment tweaks

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  output: process.env.HOSTNAME === '0.0.0.0' ? 'standalone' : undefined,
 };
 
 export default nextConfig;
